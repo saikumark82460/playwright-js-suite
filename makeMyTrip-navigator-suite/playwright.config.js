cae0a4a9c86e47504e575010ddd296f5a864d1d0
sai <saikumarkunkupudi123@gmail.com>
2024-09-17 13:55:39 +0530

    test

--- makeMyTrip-navigator-suite/playwright.config.js
+++ makeMyTrip-navigator-suite/playwright.config.js
@@ -50,10 +50,10 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
     // {
     //   name: 'webkit',
